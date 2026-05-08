a4ba77b43f6157d10bddf10b6ab769bfc497faec
FinnTheDev-9-9 <154968709+FinnTheDev-9-9@users.noreply.github.com>
2026-05-07 22:14:47 -0500

    Update APP_VERSION and Alamofire version

--- custom_components/govee/platform_app.py
+++ custom_components/govee/platform_app.py
@@ -14,12 +14,12 @@ from typing import Any, Dict
 
 import requests
 
-APP_VERSION = "7.4.22" #updated version from fork
+APP_VERSION = "7.4.30" #updated version from fork
 _LOGGER = logging.getLogger(__name__)
 
 
 def _ua() -> str:
-    return f"GoveeHome/{APP_VERSION} (com.ihoment.GoVeeSensor; build:2; iOS 16.5.0) Alamofire/5.6.4"
+    return f"GoveeHome/{APP_VERSION} (com.ihoment.GoVeeSensor; build:2; iOS 16.5.0) Alamofire/5.12.0"
 
 # Simple in-process token cache per email to avoid repeated logins
 _PLATFORM_TOKEN_CACHE: dict[str, tuple[str, str, float]] = {}
